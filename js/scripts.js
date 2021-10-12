ebee9e15a73c09da1509b6b3831c59dde8529a6a
Robby Bates <hello@robbybates.com>
2021-10-11 19:47:00 -0600

    added onchange and paallelshell

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+
+$('#reserveModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
+
+
+
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
